7a19bfc16294f15fd5770dc4ef945a7f2f75820e
wasi97 <mw7262@gmail.com>
2023-09-29 18:10:41 +0500

    database added

--- todoApp/app.js
+++ todoApp/app.js
@@ -2,7 +2,7 @@ var inp = document.getElementById("inp")
 
 var todoList = document.getElementById("todoList")
 // array declared to store todo data
-var todoData = ["dsj","lkf","jlk"]
+var todoData = []
 
 // start screen
 var startScreen = document.getElementById("startScreen");
@@ -10,84 +10,156 @@ var startScreen = document.getElementById("startScreen");
 var todoAdd = document.getElementById("todoAdd")
 // cancel todo button on start screen
 var cancelBtnStart = document.getElementById("cancelBtnStart")
+// update screen
+var updateDataScreen = document.getElementById("updateDataScreen")
+
+ // Import the functions you need from the SDKs you need
+ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
+//  import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js";
+ import { getDatabase, ref, set, onChildAdded, update, remove } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-database.js";
+ // TODO: Add SDKs for Firebase products that you want to use
+ // https://firebase.google.com/docs/web/setup#available-libraries
+
+ // Your web app's Firebase configuration
+ // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+ var firebaseConfig = {
+    apiKey: "AIzaSyCnz0xSV19Cvn9AldpPwnVlX0Iv0fHZgBg",
+    authDomain: "testwasiauth.firebaseapp.com",
+    projectId: "testwasiauth",
+    storageBucket: "testwasiauth.appspot.com",
+    messagingSenderId: "1065191757221",
+    appId: "1:1065191757221:web:9918c58570120efaf7044c",
+    measurementId: "G-CFV9JFDWYN"
+  };
+
+ // Initialize Firebase
+ var app = initializeApp(firebaseConfig);
+ var DATABASE = getDatabase(app);
+
+
+
+
+
+
+
+
 
 // render in page
-function render(dltAll){
-    todoList.innerHTML = ""
-    if(dltAll)
+// get data from database and if found some date render it on page
+function render(data){
+    todoList.innerHTML = "";
+
+    if(data)
     {
-        todoData = []
-        startScreen.classList.remove("hidden")
-        todoAdd.classList.add("hidden")
-        return
-    }
+        startScreen.classList.add("hidden")
+        todoAdd.classList.remove("hidden")
+        todoData.push(data)
+
+    } 
+
     for(var i=0; i<todoData.length;i++){
-        todoList.innerHTML += `<li class="liData"> <div class="listDataValue"> ${todoData[i]} </div>   <div class="liBtn"><button class="btn" onclick="editTodo(${i})"><i class="fa-solid fa-pen fs-1"></i></button> <button class="btn" onclick="deleteTodo(${i})"><i class="fa-solid fa-square-minus fs-1"></i></button></div></li>`
+        todoList.innerHTML += `<li class="liData"> <div class="listDataValue"> ${todoData[i].todo} </div>   <div class="liBtn"><button class="btn" onclick="editTodo(${i}, ${todoData[i].id})"><i class="fa-solid fa-pen fs-1"></i></button> <button class="btn" onclick="deleteTodo(${i}, ${todoData[i].id})"><i class="fa-solid fa-square-minus fs-1"></i></button></div></li>`
+
     }
 }
 // add to todo list
-function addTodo(){
- 
+window.addTodo = function(){
     if(inp.value === "" && startScreen.classList.contains("hidden")){
         startScreen.classList.remove("hidden")
         todoAdd.classList.add("hidden")
         cancelBtnStart.classList.remove("hidden")
-
+        
     }else
     if(inp.value === ""){
         alert("task is required");  
     } else{
         startScreen.classList.add("hidden")
         todoAdd.classList.remove("hidden");
-        todoData.push(inp.value)
+        // object creation
+        var todo = {
+            todo: inp.value,
+            id: Math.floor(Math.random(6)*1000000),
+        }
+        // data push in database and local array
+        var refer = ref(DATABASE, `todos/${todo.id}/`)
+        set(refer, todo)
         inp.value = ""
-        render();
-    }
-   
+getDataFromDatabase()    }
+    
 }
+
+// import data from server 
+
+function getDataFromDatabase(){
+    todoData = []
+    var reference = ref(DATABASE,'todos/')
+    onChildAdded(reference,function(data){
+      render(data.val())
+    })
+  }
+  window.onload = getDataFromDatabase()
+
+
 // edit in current todo task
 var todataOld;
-function editTodo(indexNum){
-    todataOld = todoData[indexNum]
+window.editTodo = function(indexNum){
+    updateDataScreen.classList.remove("hidden")
+    todoList.classList.add("hidden")
 
-    todoData[indexNum] = todoData[indexNum].innerHTML = `<div><input id="updateInp"/> <button class="btn" onclick="update(${indexNum})"><i class="fa-solid fa-pen-to-square fa-2xl"></i></button> <button class="btn" onclick="cancel(${indexNum})"><i class="fa-solid fa-xmark fa-xl"></i></button> </div>`
-    render()
-
-    
-    
+    updateDataScreen.innerHTML = `<LI class="liData"><div class="listDataValue">
+    <input type="text" id="updateInput" >
+    <button class="btn" onclick="updateDataInDB(${indexNum})">update</button>
+    <button class="btn" onclick="cancel(${indexNum})">cancel</button>
+</div></LI>` 
 }
 
-function update(indexNum){
-    var updateInp = document.getElementById("updateInp")
-    if(updateInp.value === ""){
+window.updateDataInDB = function(indexNum){
+// update input
+var updateInput = document.getElementById("updateInput")
+    if(updateInput.value === ""){
         alert("task can't be empty")
     }
     else{
-        todoData[indexNum] = updateInp.value;
-        render()
+
+        var refer = ref(DATABASE, `todos/${todoData[indexNum].id}`);
+        update(refer,{
+            id: todoData[indexNum].id,
+            todo: updateInput.value
+        })
+        updateDataScreen.classList.add("hidden")
+    todoList.classList.remove("hidden")
+        getDataFromDatabase()
+        // console.log(todoData[indexNum].id)
     }
 }
 
 // cancle on editable
-function cancel(indexNum){
-    todoData[indexNum] =  todataOld;
-    render()
+window.cancel =function(indexNum){
+    updateDataScreen.classList.add("hidden")
+    todoList.classList.remove("hidden")
 }
 
 // cancel button on add screen
-function cancelBtnSt(){
+window.cancelBtnSt = function(){
 startScreen.classList.add("hidden");
-render()
 todoAdd.classList.remove("hidden");
+render()
 }
 // delete a todo
-function deleteTodo(indexNum){
-    todoData.splice(indexNum, 1)
-    render()
+window.deleteTodo = function(indexNum,id){
+    var refer = ref(DATABASE, `todos/${id}`)
+    remove(refer);
+    todoData = []
+    getDataFromDatabase()
 }
 
-render()
-
+// delete all
+window.deleteAllData = function (){
+    var refer = ref(DATABASE,"todos/")
+    remove(refer)
+    console.log("first")
+}
+// getDataFromDatabase()
 
 
 
